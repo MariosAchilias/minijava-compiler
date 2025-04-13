52104ab0784a5229d6b981c0443ce606d3c8ad91
MariosAchilias <marios.axilias@protonmail.com>
2025-04-13 12:32:23 +0300

    First working version

--- ExpressionEvaluator/ExpressionEvaluator.java
+++ ExpressionEvaluator/ExpressionEvaluator.java
@@ -19,7 +19,67 @@ public class ExpressionEvaluator {
         return value;
     }
 
-    private int Expr() throws IOException, ParseError {
-        return 0;
+    private void consume(int symbol) throws IOException, ParseError {
+        if (lookahead == symbol)
+            lookahead = in.read();
+        else
+            throw new ParseError();
     }
+
+    private boolean isDigit(int c) {
+        return '0' <= c && c <= '9';
+    }
+
+    private int evalDigit(int c) {
+        return c - '0';
+    }
+
+    private int Expr() throws IOException, ParseError {
+        int value = PowExpr();
+        return ExprTail(value);
+    }
+
+    private int ExprTail(int leftop) throws IOException, ParseError {
+        int rightop;
+        switch (lookahead) {
+            case '+':
+                consume('+');
+                rightop = PowExpr();
+                return ExprTail(leftop + rightop);
+            case '-':
+                consume('-');
+                rightop = PowExpr();
+                return ExprTail(leftop - rightop);
+            default:
+                return leftop;
+        }
+    }
+
+    private int PowExpr() throws IOException, ParseError {
+        int val = Factor();
+        return PowExprTail(val);
+    }
+
+    private int PowExprTail(int left) throws IOException, ParseError {
+        if (lookahead == '*') {
+            consume('*');
+            if (lookahead == '*') {
+                consume('*');
+                int val = Factor();
+                return PowExprTail((int) Math.pow(left, val));
+            }
+        }
+
+        return left;
+    }
+
+    private int Factor() throws IOException, ParseError {
+        if (isDigit(lookahead)) {
+            int value = evalDigit(lookahead);
+            consume(lookahead);
+            return value;
+        }
+        throw new ParseError();
+    }
+
 }
