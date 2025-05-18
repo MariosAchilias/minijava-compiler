657668b3ac49b0afd9e3fb417eeb81234d6b820e
MariosAchilias <marios.axilias@protonmail.com>
2025-05-18 22:05:33 +0300

    Add semantic analyzer skeleton

---
+++ MiniJavaSemanticAnalyzer/Main.java
@@ -0,0 +1,31 @@
+import syntaxtree.*;
+import visitor.*;
+import java.io.*;
+
+class Main {
+    public static void main (String [] args){
+        FileInputStream fis = null;
+        try{
+            fis = new FileInputStream(args[0]);
+            MiniJavaParser parser = new MiniJavaParser(fis);
+            System.err.println("Program parsed successfully.");
+            SemanticAnalysisVisitor eval = new SemanticAnalysisVisitor();
+            Goal root = parser.Goal();
+            System.out.println(root.accept(eval, null));
+        }
+        catch(ParseException ex){
+            System.out.println(ex.getMessage());
+        }
+        catch(FileNotFoundException ex){
+            System.err.println(ex.getMessage());
+        }
+        finally{
+            try{
+                if(fis != null) fis.close();
+            }
+            catch(IOException ex){
+                System.err.println(ex.getMessage());
+            }
+        }
+    }
+}
---
+++ MiniJavaSemanticAnalyzer/SemanticAnalysisVisitor.java
@@ -0,0 +1,5 @@
+import syntaxtree.*;
+import visitor.GJDepthFirst;
+
+public class SemanticAnalysisVisitor extends GJDepthFirst<Integer, Integer> {
+}
