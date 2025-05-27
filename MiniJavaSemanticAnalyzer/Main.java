7567caf53f93032572c5ea5dedf73ca0ed9d0df7
MariosAchilias <marios.axilias@protonmail.com>
2025-05-27 18:11:58 +0300

    Change main to take multiple files as arguments

--- MiniJavaSemanticAnalyzer/Main.java
+++ MiniJavaSemanticAnalyzer/Main.java
@@ -4,33 +4,35 @@ import java.io.*;
 class Main {
     public static void main (String [] args) throws Exception{
         FileInputStream fis = null;
-        try{
-            fis = new FileInputStream(args[0]);
-            MiniJavaParser parser = new MiniJavaParser(fis);
-            Goal root = parser.Goal();
-            System.err.println("Program parsed successfully.");
+        for (String fileName : args) {
+            try {
+                fis = new FileInputStream(fileName);
+                MiniJavaParser parser = new MiniJavaParser(fis);
+                Goal root = parser.Goal();
+                System.err.println(fileName + " parsed successfully.");
 
-            SymbolTableBuildVisitor populateSymbolTable = new SymbolTableBuildVisitor();
-            root.accept(populateSymbolTable, null);
+                SymbolTableBuildVisitor populateSymbolTable = new SymbolTableBuildVisitor();
+                root.accept(populateSymbolTable, null);
 
-            SymbolTable symbolTable = SymbolTable.getInstance();
-            symbolTable.printOffsets();
-//            symbolTable.prettyPrint();
-            // TypeCheckVisitor typeCheck = new TypeCheckVisitor(populateSymbolTable.symbolTable);
-            // root.accept(populateSymbolTable, null);
-        }
-        catch(ParseException ex){
-            System.out.println(ex.getMessage());
-        }
-        catch(FileNotFoundException ex){
-            System.err.println(ex.getMessage());
-        }
-        finally{
-            try{
-                if(fis != null) fis.close();
+                SymbolTable symbolTable = SymbolTable.getInstance();
+                symbolTable.printOffsets();
+                TypeCheckVisitor typeCheck = new TypeCheckVisitor();
+                root.accept(populateSymbolTable, null);
+
             }
-            catch(IOException ex){
+            catch(ParseException ex){
+                System.out.println(ex.getMessage());
+            }
+            catch(FileNotFoundException ex){
                 System.err.println(ex.getMessage());
+            }
+            finally{
+                try{
+                    if(fis != null) fis.close();
+                }
+                catch(IOException ex){
+                    System.err.println(ex.getMessage());
+                }
             }
         }
     }
