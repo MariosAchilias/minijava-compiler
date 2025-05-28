0edc99a5462259ded99d3801b4f5e7eedfd81a10
MariosAchilias <marios.axilias@protonmail.com>
2025-05-28 17:16:15 +0300

    Add array types

--- MiniJavaSemanticAnalyzer/VarType.java
+++ MiniJavaSemanticAnalyzer/VarType.java
@@ -6,9 +6,12 @@ public enum VarType {
     BOOLEAN_ARRAY;
 
     public static VarType getType(String type) throws Exception {
+        System.out.println("type " + type);
         switch (type) {
             case "int": return INTEGER;
+            case "int[]": return INTEGER_ARRAY;
             case "boolean": return BOOLEAN;
+            case "boolean[]": return BOOLEAN_ARRAY;
             default: return CLASS_INSTANCE;
         }
     }
