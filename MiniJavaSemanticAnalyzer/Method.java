3b6ca65df9030f9b101f7cef998c4bbef6e08c48
MariosAchilias <marios.axilias@protonmail.com>
2025-05-30 13:32:21 +0300

    Modify Method.compatibleParameters to accept subtypes

--- MiniJavaSemanticAnalyzer/Method.java
+++ MiniJavaSemanticAnalyzer/Method.java
@@ -22,12 +22,30 @@ public class Method extends Symbol{
         return localVars;
     }
 
-    public static boolean compatibleParameters(ArrayList<Variable> parameters, ArrayList<Variable> parameters_) {
-        if (parameters.size() != parameters_.size())
+    public static boolean compatibleParameters(ArrayList<Variable> methodParameters, ArrayList<Variable> testParameters) {
+        if (methodParameters.size() != testParameters.size())
             return false;
-        for (int i = 0; i < parameters.size(); i++)
-            if (!parameters.get(i).varType.equals(parameters_.get(i).varType))
+        for (int i = 0; i < methodParameters.size(); i++) {
+            String type = methodParameters.get(i).varType;
+            // if parameter is of user-defined type, check if given parameter is a subtype -- a bit ugly TODO: rewrite
+            if (!(type.equals("int") || type.equals("boolean") || type.equals("int[]") || type.equals("boolean[]"))) {
+                boolean flag = false;
+                for (Class c = SymbolTable.getInstance().getClass(testParameters.get(i).varType); c != null; c = c.getParent()) {
+                    if (c.id.equals(type)) {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag)
+                    continue;
+                else
+                    return false;
+            }
+
+            if (!methodParameters.get(i).varType.equals(testParameters.get(i).varType))
                 return false;
+
+        }
 
         return true;
     }
