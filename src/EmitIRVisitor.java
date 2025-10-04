2054f7d9ace3cf5844d2b48965621f36bfecf833
Marios Achilias <marios.axilias@gmail.com>
2025-10-05 01:56:29 +0300

    add emitter skeleton

---
+++ src/EmitIRVisitor.java
@@ -0,0 +1,129 @@
+import Parser.syntaxtree.*;
+import Parser.visitor.*;
+import Exceptions.SemanticException;
+import SymbolTable.*;
+import SymbolTable.Class;
+import Emitter.*;
+
+class EmittIrVisitor extends GJDepthFirst<String, String>{
+    SymbolTable symbolTable;
+    public EmitIrVisitor(SymbolTable symbolTable) {
+        this.symbolTable = symbolTable;
+    }
+    /**
+     * f0 -> "class"
+     * f1 -> Identifier()
+     * f2 -> "{"
+     * f3 -> "public"
+     * f4 -> "static"
+     * f5 -> "void"
+     * f6 -> "main"
+     * f7 -> "("
+     * f8 -> "String"
+     * f9 -> "["
+     * f10 -> "]"
+     * f11 -> Identifier()
+     * f12 -> ")"
+     * f13 -> "{"
+     * f14 -> ( VarDeclaration() )*
+     * f15 -> ( Statement() )*
+     * f16 -> "}"
+     * f17 -> "}"
+     */
+    @Override
+    public String visit(MainClass n, String argu) throws Exception {
+        return null;
+    }
+
+    /**
+     * f0 -> "class"
+     * f1 -> Identifier()
+     * f2 -> "{"
+     * f3 -> ( VarDeclaration() )*
+     * f4 -> ( MethodDeclaration() )*
+     * f5 -> "}"
+     */
+    @Override
+    public String visit(ClassDeclaration n, String argu) throws Exception {
+        return null;
+    }
+
+    /**
+     * f0 -> "class"
+     * f1 -> Identifier()
+     * f2 -> "extends"
+     * f3 -> Identifier()
+     * f4 -> "{"
+     * f5 -> ( VarDeclaration() )*
+     * f6 -> ( MethodDeclaration() )*
+     * f7 -> "}"
+     */
+    @Override
+    public String visit(ClassExtendsDeclaration n, String argu) throws Exception {
+        return null;
+    }
+
+    /**
+     * f0 -> Type()
+     * f1 -> Identifier()
+     * f2 -> ";"
+     */
+    public String visit(VarDeclaration n, String argu) throws Exception {
+        return null;
+    }
+
+    /**
+     * f0 -> "public"
+     * f1 -> Type()
+     * f2 -> Identifier()
+     * f3 -> "("
+     * f4 -> ( FormalParameterList() )?
+     * f5 -> ")"
+     * f6 -> "{"
+     * f7 -> ( VarDeclaration() )*
+     * f8 -> ( Statement() )*
+     * f9 -> "return"
+     * f10 -> Expression()
+     * f11 -> ";"
+     * f12 -> "}"
+     */
+    @Override
+    public String visit(MethodDeclaration n, String className) throws Exception {
+        return null;
+    }
+
+    /**
+     * f0 -> Type()
+     * f1 -> Identifier()
+     */
+    @Override
+    public String visit(FormalParameter n, String argu) throws Exception {
+        return null;
+    }
+
+
+    public String visit(ArrayType n, String argu) throws Exception {
+        return n.f0.accept(this, null);
+    }
+
+    public String visit(BooleanArrayType n, String argu) {
+        return "boolean[]";
+    }
+
+    public String visit(IntegerArrayType n, String argu) {
+        return "int[]";
+    }
+
+    public String visit(BooleanType n, String argu) {
+        return "boolean";
+    }
+
+    public String visit(IntegerType n, String argu) {
+        return "int";
+    }
+
+    @Override
+    public String visit(Identifier n, String argu) {
+        return n.f0.toString();
+    }
+}
---
+++ src/Emitter/Emitter.java
@@ -0,0 +1,5 @@
+package Emitter;
+
+public class Emitter {
+    
+}
